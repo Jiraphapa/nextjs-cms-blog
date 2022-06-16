6df8724f31cdbfe94af87b2e31004af97ef65073
Jiraphapa <jira.jiravaraphan@gmail.com>
2022-06-16 09:54:58 +0200

    decorate PostWidget

--- components/PostWidget.tsx
+++ components/PostWidget.tsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import moment from 'moment'
+import React, {useState, useEffect} from 'react'
+import Link from 'next/link'
 
-const PostWidget = () => {
+import { getRecentPosts } from '../services'
+import { getSimilarPosts } from '../services'
+
+const PostWidget: React.FC<any> = ({categories, slug}) => {
+  const [relatedPosts, setRelatedPosts] = useState([]);
+
+  useEffect(() => {
+    if(slug){
+      getSimilarPosts(categories, slug)
+        .then((result) => setRelatedPosts(result))
+    } else {
+      getRecentPosts()
+      .then((result) => setRelatedPosts(result))
+    }
+  
+    console.log(relatedPosts);
+    
+  }, [slug])
+
   return (
-    <div className="bg-white shadow-lg rounded-lg p-8 pb-12 mb-8">PostWidget</div>
+    <div className="bg-white shadow-lg rounded-lg p-8 pb-12 mb-8">
+      <h3>
+        {slug? 'Related Posts' : 'Recent Posts'}
+      </h3>
+    </div>
   )
+  
+  
 }
 
 export default PostWidget
