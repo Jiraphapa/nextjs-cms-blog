1ce9c5c8178fb9b99e82ee60bf2e26e418788e2a
Jiraphapa <jira.jiravaraphan@gmail.com>
2022-06-16 09:55:51 +0200

    add fucntion to ferch recent posts

--- services/index.ts
+++ services/index.ts
@@ -32,8 +32,47 @@ export const getPosts = async () => {
         }
       }
       
-    `
+    `;
 
     const result = await request(graphqlAPI, query);
     return result.postsConnection.edges;
+}
+
+
+export const getRecentPosts = async() => {
+    const query = gql `
+        query GetPostDetails() {
+        posts(orderBy: publishedAt_ASC, last: 3) {
+          slug
+          createdAt
+          title
+          featuredImage {
+            url
+          }
+        }
+      }
+    `;
+
+    const result = await request(graphqlAPI, query);
+    return result.posts;
+}
+
+export const getSimilarPosts = async(categories: any, slug: any) => {
+    const query = gql `
+        query GetPostDetails($slug: String!, $categories: [String!]) {
+            posts (
+                where: {slug_not: $slug, AND [categories_some: {slug_in: $categories}]}
+                last: 3
+            ) {
+                slug
+                createdAt
+                title
+                featuredImage {
+                    url
+                }
+            }
+        }
+    `;
+    const result = await request(graphqlAPI, query);
+    return result.posts;
 }
