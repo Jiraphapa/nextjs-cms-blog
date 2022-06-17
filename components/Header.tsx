9c6de0a9bcf7c45f15ad9714d92fd616ae576db7
Jiraphapa <jira.jiravaraphan@gmail.com>
2022-06-17 09:48:40 +0200

    add dynamic categories

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,17 @@
-import React, {useContext} from 'react'
+import React, {useContext, useState, useEffect} from 'react'
 
 import Link from 'next/link';
-
-const categories = [{name: 'Security', slug: 'security' }, {name: 'Web Development', slug: 'web-dev'}, {name: 'AWS', slug: 'aws'}, {name: 'Architecture', slug: 'architecture'}, {name: 'Adventure', slug: 'adventure'}, {name: 'Tech', slug: 'tech'}]
+import { getCategories } from '../services';
 
 const Header = () => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    getCategories().then((newCategories) => {
+      setCategories(newCategories);
+    });
+  }, []);
+
   return (
     <div className="container mx-auto px-10 mb-8">
       <div className="border-b w-full inline-block border-blue-400 py-8">
