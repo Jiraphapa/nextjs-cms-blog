9325c6d56ad83aafd650093c9c647d83dae82cd4
Jiraphapa <jira.jiravaraphan@gmail.com>
2022-06-21 06:36:10 +0200

    refactor

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -8,7 +8,7 @@ const PostDetails: React.FC<any> = ({ post }) => {
   console.log("whatt", post);
   return (
       <div className="container mx-auto px-10 mb-8">
-        {/* <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+        <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
           <div className="col-span-1 lg:col-span-8">
             <PostDetail post={post}/>
             <Author author={post.author}/>
@@ -21,7 +21,7 @@ const PostDetails: React.FC<any> = ({ post }) => {
               <Categories />
             </div>
           </div>
-        </div> */}
+        </div>
       </div>
   );
 };
