df3ed7e654b17aa6cfbb0383541a18cc96507fae
Jiraphapa <jira.jiravaraphan@gmail.com>
2022-06-24 14:45:40 +0200

    decorate CommentsForm component with comment and button

--- components/CommentsForm.tsx
+++ components/CommentsForm.tsx
@@ -9,9 +9,23 @@ const CommentsForm: React.FC<any> = ({slug}) => {
   const emailEl = useRef();
   const storeDataEl = useRef();
 
+  const handlePostSubmission = () => {}
+
   return (
     <div className="bg-white shadow-lg rounded-lg p-8 pb-12 mb-8">
-      <h1>CommentsForm</h1>
+      <h3 className="text-xl mb-8 font-semibold border-b pb-4">Leave a Reply</h3>
+      <div className="grid grid-cols-1 gap-4 mb-4">
+        <textarea ref={commentEl} className="p-4 outline-none w-full rounded-lg h-40 focus:ring-2 focus:ring-gray-200 bg-gray-100 text-gray-700" name="comment" placeholder="Comment" />
+      </div>
+      <div className="grid lg:grid-cols-2 grid-cols-1 gap-4 mb-4">
+       <input type="text" ref={nameEl} className="py-2 px-4 outline-none w-full rounded-lg focus:ring-2 focus:ring-gray-200 bg-gray-100 text-gray-700" name="name" placeholder="Name"></input>
+       <input type="email" ref={emailEl} className="py-2 px-4 outline-none w-full rounded-lg focus:ring-2 focus:ring-gray-200 bg-gray-100 text-gray-700" name="email" placeholder="Email"></input>
+      </div>
+      {error && <p className="text-xs text-red-500">All fields are mandatory</p>}
+      <div className="mt-8">
+        <button type="button" onClick={handlePostSubmission} className="transition duration-500 ease hover:bg-indigo-900 inline-block bg-pink-600 text-lg font-medium rounded-full text-white px-8 py-3 cursor-pointer">Post Comment</button>
+        {showSuccessMessage && <span className="text-xl float-right font-semibold mt-3 text-green-500">Comment submitted for review</span>}
+      </div>
     </div>
   )
 }
