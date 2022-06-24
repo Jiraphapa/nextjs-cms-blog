c78b17984f54f9d6886a4a07817891851bb5bc0d
Jiraphapa <jira.jiravaraphan@gmail.com>
2022-06-24 13:58:55 +0200

    decorate CommentsForm

--- components/CommentsForm.tsx
+++ components/CommentsForm.tsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, {useRef, useState, useEffect} from 'react'
 
-const CommentsForm = () => {
+const CommentsForm: React.FC<any> = ({slug}) => {
+  const [error, setError] = useState(false);
+  const [localStorage, setlocalStorage] = useState(null)
+  const [showSuccessMessage, setshowSuccessMessage] = useState(false)
+  const commentEl = useRef();
+  const nameEl = useRef();
+  const emailEl = useRef();
+  const storeDataEl = useRef();
+
   return (
-    <h1>CommentsForm</h1>
+    <div className="bg-white shadow-lg rounded-lg p-8 pb-12 mb-8">
+      <h1>CommentsForm</h1>
+    </div>
   )
 }
 
